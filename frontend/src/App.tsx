5fca2666ebfb61bd6cb2ca1f40b53227b391be05
Rinae Hyun <rnhyun.dream@gmail.com>
2024-08-28 10:44:51 +0200

    Create placeholder for header and footer

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,23 @@
 import './App.css'
 import {Route, Routes} from "react-router-dom";
-import WelcomePage from "./pages/welcompage/WelcomePage.tsx";
+import Home from "./pages/Home/Home.tsx";
 import TrackingPage from "./pages/trackingpage/TrackingPage.tsx";
+import Header from "./layouts/Header/Header.tsx";
+import Footer from "./layouts/Footer/Footer.tsx";
 
 function App() {
 
     return (
-        <main>
-            <Routes>
-                <Route path={"/"} element={<WelcomePage />}/>
-                <Route path={"/tracking"} element={<TrackingPage />}/>
-            </Routes>
-        </main>
+        <div className={"app"}>
+            <Header />
+            <main>
+                <Routes>
+                    <Route path={"/"} element={<Home />}/>
+                    <Route path={"/tracking"} element={<TrackingPage />}/>
+                </Routes>
+            </main>
+            <Footer />
+        </div>
     )
 }
 
---
+++ frontend/src/layouts/Header/Header.tsx
@@ -0,0 +1,9 @@
+import './Header.css'
+
+export default function Header() {
+    return(
+        <header>
+            <h3 style={{margin: 0, paddingLeft: "20px"}}>Logo</h3>
+        </header>
+    )
+}
