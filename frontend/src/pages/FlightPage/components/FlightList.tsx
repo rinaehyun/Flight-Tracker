ad7444fde7860c664ef1a58247738c0faf2af949
Rinae Hyun <rnhyun.dream@gmail.com>
2024-08-28 16:07:15 +0200

    Create a function for String Airline name

--- frontend/src/pages/FlightPage/components/FlightList.tsx
+++ frontend/src/pages/FlightPage/components/FlightList.tsx
@@ -2,6 +2,7 @@ import './FlightList.css'
 import {Flight} from "../../../types/model/dataType.ts";
 import {Airline} from "../../../types/enum.ts";
 import FlightTakeoffIcon from '@mui/icons-material/FlightTakeoff';
+import {capitalizeFirstLetter} from "../../../utils/funtioncs.ts";
 
 type FlightListProps = {
     data: Flight[]
@@ -14,7 +15,7 @@ export default function FlightList({ data }: FlightListProps) {
                 <div key={flight.id} className={"flight-card"}>
                     <div className={"flight-card-airline"}>
                         <FlightTakeoffIcon sx={{ fontSize: "25px"}}/>
-                        <p>{Airline[flight.airline as unknown as keyof typeof Airline]}</p>
+                        <p>{capitalizeFirstLetter(Airline[flight.airline as unknown as keyof typeof Airline])}</p>
                         <p>{flight.flightCode}</p>
                     </div>
                     <div className={"flight-card-detail"}>
---
+++ frontend/src/utils/funtioncs.ts
@@ -0,0 +1,7 @@
+export function capitalizeFirstLetter(string: string) {
+    const splitStr: string[] = string.toLowerCase().split(' ');
+    for (let i = 0; i < splitStr.length; i++) {
+        splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);
+    }
+    return splitStr.join(' ');
+}
