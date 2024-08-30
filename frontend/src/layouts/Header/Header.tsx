e77aa93700b6110130f30a30cadc43447c22b242
Rinae Hyun <rnhyun.dream@gmail.com>
2024-08-30 14:15:25 +0200

    Extend Header with navigations

--- frontend/src/layouts/Header/Header.tsx
+++ frontend/src/layouts/Header/Header.tsx
@@ -1,15 +1,26 @@
 import './Header.css'
 import FlightTakeoffIcon from '@mui/icons-material/FlightTakeoff';
+import {Link} from "react-router-dom";
 
 export default function Header() {
 
     return(
         <header>
             <FlightTakeoffIcon
-                sx={{ fontSize: "25px"}}
-                onClick={() => window.location.href='/'}
-                style={{ marginLeft: "20px", cursor: "pointer"}}
+                sx={{fontSize: "35px"}}
+                onClick={() => window.location.href = '/'}
+                style={{marginLeft: "20px", cursor: "pointer"}}
             />
+            <nav>
+                <ul>
+                    <li>
+                        <Link to={"/"}>Home</Link>
+                    </li>
+                    <li>
+                        <Link to={"/flight"}>Flight</Link>
+                    </li>
+                </ul>
+            </nav>
         </header>
     )
 }
