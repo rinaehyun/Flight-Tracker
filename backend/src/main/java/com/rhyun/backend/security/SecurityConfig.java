ba9eea63245547947ad66ca4f32655493c57a056
Rinae Hyun <rnhyun.dream@gmail.com>
2024-09-04 18:38:26 +0200

    Create a SecurityConfig in BE and login function in FE

---
+++ backend/src/main/java/com/rhyun/backend/security/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.rhyun.backend.security;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+
+public class SecurityConfig {
+
+    @Value("${app.url}")
+    private String appUrl;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.ALWAYS))
+                .exceptionHandling(e -> e.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)))
+                .oauth2Login(o -> o.defaultSuccessUrl(appUrl))
+                .authorizeHttpRequests(a -> a
+                        .requestMatchers(HttpMethod.GET, "/api/flight").permitAll());
+
+        return httpSecurity.build();
+    }
+}
--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,10 +1,17 @@
 import {Link} from "react-router-dom";
 
 export default function WelcomePage() {
+
+    const login = () => {
+        const host = window.location.host === 'localhost:5173' ? 'http://localhost:8080' : window.location.origin;
+
+        window.open(host + '/oauth2/authorization/github', '_self');
+    }
+
     return(
         <>
             <h2>Welcome to Flight App!</h2>
-            <Link to={"/flight"}>Login</Link>
+            <Link to={"/flight"} onClick={login}>Login with Github</Link>
         </>
     )
 }
