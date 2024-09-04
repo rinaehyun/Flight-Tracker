c5459029c7059396f416250cbdd230b027a71bb0
Rinae Hyun <rnhyun.dream@gmail.com>
2024-09-04 11:46:20 +0200

    Create a notification when the user click the update button

--- frontend/src/pages/FlightDetailPage/FlightDetailPage.tsx
+++ frontend/src/pages/FlightDetailPage/FlightDetailPage.tsx
@@ -2,10 +2,11 @@ import './FlightDetailPage.css';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import {NavigateFunction, useNavigate, useParams} from "react-router-dom";
 import {FormEvent, useEffect, useState} from "react";
-import {Box} from "@mui/material";
+import {Alert, Box, IconButton, Stack} from "@mui/material";
 import {NewFlight} from "../../types/model/dataType.ts";
 import FlightForm from "../AddFlightPage/components/FlightForm.tsx";
 import axios from "axios";
+import CloseIcon from '@mui/icons-material/Close';
 
 type FlightDetailPageProps = {
     fetchAllFlights: () => void,
@@ -13,6 +14,8 @@ type FlightDetailPageProps = {
 
 export default function FlightDetailPage({ fetchAllFlights }: FlightDetailPageProps) {
     const [editable, setEditable ] = useState<boolean>(false);
+    const [showNotification, setShowNotification] = useState<boolean>(false);
+
     const [newFlight, setNewFlight] = useState<NewFlight>({
         flightCode: "",
         airline: "",
@@ -48,7 +51,11 @@ export default function FlightDetailPage({ fetchAllFlights }: FlightDetailPagePr
     const updateFlight = (id: string, flight: NewFlight) => {
         console.log(flight)
         axios.put(`/api/flight/${id}`, flight)
-            .then(response => response.status === 200 && fetchAllFlights())
+            .then(response => {
+                // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+                response.status === 200 && fetchAllFlights();
+                setShowNotification(true);
+            })
             .catch(error => console.log(error.response.data))
     }
 
@@ -58,8 +65,38 @@ export default function FlightDetailPage({ fetchAllFlights }: FlightDetailPagePr
         setEditable(false)
     }
 
+    useEffect(() => {
+        if (showNotification) {
+            const timer = setTimeout(() => {
+                setShowNotification(false);
+            }, 5000);
+
+            return () => clearTimeout(timer);
+        }
+    }, [showNotification]);
+
     return (
         <article className={"flight-details"}>
+            {showNotification &&
+                <Stack className={"stack-alert"}>
+                   <Alert
+                        severity="success"
+                        action={
+                            <IconButton
+                                aria-label="close"
+                                color="inherit"
+                                size="small"
+                                onClick={() => {
+                                    setShowNotification(false);
+                                }}
+                            >
+                                <CloseIcon fontSize="inherit" />
+                            </IconButton>
+                        }
+                    >Flight Data has been updated.</Alert>
+                </Stack>
+            }
+
             <div className={"top-buttons"}>
                 <Box
                     sx={{
