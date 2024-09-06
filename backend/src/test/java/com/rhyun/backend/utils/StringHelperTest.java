7d4ae24f6a13979329965befd50cd7c4b2e7b17c
Rinae Hyun <rnhyun.dream@gmail.com>
2024-09-06 17:56:25 +0200

    Cover the case of empty input to String Helper

--- backend/src/test/java/com/rhyun/backend/utils/StringHelperTest.java
+++ backend/src/test/java/com/rhyun/backend/utils/StringHelperTest.java
@@ -3,12 +3,13 @@ package com.rhyun.backend.utils;
 import org.junit.jupiter.api.Test;
 
 import static com.rhyun.backend.utils.StringHelper.capitalizeFirstLetter;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class StringHelperTest {
 
     @Test
-    void capitalizeFirstLetterTest() {
+    void capitalizeFirstLetterTest_whenInputHasValue_thenReturnString() {
         // GIVEN
         String input1 = "HeLLo, wORLD!";
         String input2 = "FRIDAY";
@@ -23,5 +24,18 @@ class StringHelperTest {
         assertEquals("Hello, World!", actual1);
         assertEquals("Friday", actual2);
         assertEquals("Take A Rest..", actual3);
+    }
+
+    @Test
+    void capitalizeFirstLetterTest_whenInputIsEmpty_thenReturnNothing() {
+        // GIVEN
+        String input1 = "  ";
+
+        // WHEN
+        String actual1 = capitalizeFirstLetter(input1);
+
+        // THEN
+        assertEquals("", actual1);
+        assertThat(actual1).isEmpty();
     }
 }
