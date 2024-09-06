c9973fa20b05356400a758660988db63725b9243
Rinae Hyun <rnhyun.dream@gmail.com>
2024-09-06 17:24:33 +0200

    Write Unit tests for getAirportOptions()

--- backend/src/test/java/com/rhyun/backend/airport/service/AirportServiceTest.java
+++ backend/src/test/java/com/rhyun/backend/airport/service/AirportServiceTest.java
@@ -1,5 +1,6 @@
 package com.rhyun.backend.airport.service;
 
+import com.rhyun.backend.airport.dto.GetAirportDto;
 import com.rhyun.backend.airport.model.Airport;
 import com.rhyun.backend.airport.model.AirportAddress;
 import com.rhyun.backend.airport.model.AirportTimeZone;
@@ -19,6 +20,11 @@ class AirportServiceTest {
     private final AirportRepository airportRepository = mock(AirportRepository.class);
     private final AirportService airportService = new AirportService(airportRepository);
 
+    Airport airport1 = new Airport("123", "GDANSK", "GDN", new GeoCode(54, 18),
+            new AirportAddress("POLAND"), new AirportTimeZone("+02:00"));
+    Airport airport2 = new Airport("456", "GOTEBORG", "GOT", new GeoCode(57, 12),
+            new AirportAddress("SWEDEN"), new AirportTimeZone("+02:00"));
+
     @Test
     void getAllAirportsTest_whenDBIsEmpty_thenReturnEmptyList() {
         // GIVEN
@@ -36,11 +42,6 @@ class AirportServiceTest {
     @Test
     void getAllAirportsTest_whenDBHasData_thenReturnListOfAirports() {
         // GIVEN
-        Airport airport1 = new Airport("123", "GDANSK", "GDN", new GeoCode(54, 18),
-                new AirportAddress("POLAND"), new AirportTimeZone("+02:00"));
-        Airport airport2 = new Airport("456", "GOTEBORG", "GOT", new GeoCode(57, 12),
-                new AirportAddress("SWEDEN"), new AirportTimeZone("+02:00"));
-
         List<Airport> airports = List.of(airport1, airport2);
         when(airportRepository.findAll()).thenReturn(airports);
 
@@ -49,6 +50,38 @@ class AirportServiceTest {
 
         // THEN
         List<Airport> expected = List.of(airport1, airport2);
+
+        assertEquals(expected, actual);
+        verify(airportRepository, times(1)).findAll();
+    }
+
+    @Test
+    void getAirportOptionsTest_whenDBIsEmpty_thenReturnEmptyList () {
+        // GIVEN
+        List<Airport> airports = new ArrayList<>();
+        when(airportRepository.findAll()).thenReturn(airports);
+
+        // WHEN
+        List<GetAirportDto> actual = airportService.getAirportOptions();
+
+        // THEN
+        assertThat(actual).isEmpty();
+        verify(airportRepository, times(1)).findAll();
+    }
+
+    @Test
+    void getAirportOptionsTest_whenDBHasData_thenReturnListOfAirports () {
+        // GIVEN
+        List<Airport> airports = List.of(airport1, airport2);
+        when(airportRepository.findAll()).thenReturn(airports);
+
+        // WHEN
+        List<GetAirportDto> actual = airportService.getAirportOptions();
+
+        // THEN
+        GetAirportDto airportDto1 = new GetAirportDto("GDN", "Gdansk, Poland");
+        GetAirportDto airportDto2 = new GetAirportDto("GOT", "Goteborg, Sweden");
+        List<GetAirportDto> expected = List.of(airportDto1, airportDto2);
 
         assertEquals(expected, actual);
         verify(airportRepository, times(1)).findAll();
