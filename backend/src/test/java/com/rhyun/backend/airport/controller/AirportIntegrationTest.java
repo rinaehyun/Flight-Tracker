a3c6550b83e77dcdc6ca8b04c74926c1d5c325ea
Rinae Hyun <rnhyun.dream@gmail.com>
2024-09-06 17:36:49 +0200

    Write integration tests for getAirportOptions()

--- backend/src/test/java/com/rhyun/backend/airport/controller/AirportIntegrationTest.java
+++ backend/src/test/java/com/rhyun/backend/airport/controller/AirportIntegrationTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -24,7 +25,15 @@ class AirportIntegrationTest {
     @Autowired
     private AirportRepository airportRepository;
 
+
+    Airport airport1 = new Airport("123", "GDANSK", "GDN", new GeoCode(54, 18),
+            new AirportAddress("POLAND"), new AirportTimeZone("+02:00"));
+    Airport airport2 = new Airport("456", "GOTEBORG", "GOT",
+            new GeoCode(57, 12), new AirportAddress("SWEDEN"), new AirportTimeZone("+02:00"));
+
+
     @Test
+    @DirtiesContext
     void getAllAirportsTest_whenDBIsEmpty_thenReturnEmptyList() throws Exception {
         // GIVEN
 
@@ -36,53 +45,86 @@ class AirportIntegrationTest {
     }
 
     @Test
+    @DirtiesContext
     void getAllAirportsTest_whenDBHasData_thenReturnListOfAirports() throws Exception {
         // GIVEN
-        Airport airport1 = new Airport("123", "GDANSK", "GDN", new GeoCode(54, 18),
-                new AirportAddress("POLAND"), new AirportTimeZone("+02:00"));
-        Airport airport2 = new Airport("456", "GOTEBORG", "GOT",
-                new GeoCode(57, 12), new AirportAddress("SWEDEN"), new AirportTimeZone("+02:00"));
-
         airportRepository.save(airport1);
         airportRepository.save(airport2);
 
         // WHEN
         mockMvc.perform(get("/api/airport"))
+            // THEN
+            .andExpect(status().isOk())
+            .andExpect(content().json("""
+                [
+                    {
+                        "id": "123",
+                        "name": "GDANSK",
+                        "iataCode": "GDN",
+                        "geoCode": {
+                            "latitude": 54,
+                            "longitude": 18
+                        },
+                        "address": {
+                            "countryName": "POLAND"
+                        },
+                        "timeZone": {
+                            "offSet": "+02:00"
+                        }
+                    },
+                    {
+                        "id": "456",
+                        "name": "GOTEBORG",
+                        "iataCode": "GOT",
+                        "geoCode": {
+                            "latitude": 57,
+                            "longitude": 12
+                        },
+                        "address": {
+                            "countryName": "SWEDEN"
+                        },
+                        "timeZone": {
+                            "offSet": "+02:00"
+                        }
+                    }
+                ]
+            """));
+    }
+
+    @Test
+    @DirtiesContext
+    void getAirportOptionsTest_whenDBIsEmpty_thenReturnEmptyList() throws Exception {
+        // GIVEN
+
+        // WHEN
+        mockMvc.perform(get("/api/airport/options-for-input"))
                 // THEN
                 .andExpect(status().isOk())
-                .andExpect(content().json("""
-                    [
-                        {
-                            "id": "123",
-                            "name": "GDANSK",
-                            "iataCode": "GDN",
-                            "geoCode": {
-                                "latitude": 54,
-                                "longitude": 18
-                            },
-                            "address": {
-                                "countryName": "POLAND"
-                            },
-                            "timeZone": {
-                                "offSet": "+02:00"
-                            }
-                        },
-                        {
-                            "id": "456",
-                            "name": "GOTEBORG",
-                            "iataCode": "GOT",
-                            "geoCode": {
-                                "latitude": 57,
-                                "longitude": 12
-                            },
-                            "address": {
-                                "countryName": "SWEDEN"
-                            },
-                            "timeZone": {
-                                "offSet": "+02:00"
-                            }
-                        }
-                    ]
-                """));
+                .andExpect(content().json("[]"));
+    }
+
+    @Test
+    @DirtiesContext
+    void getAirportOptionsTest_whenDBHasData_thenReturnListOfAirports() throws Exception {
+        // GIVEN
+        airportRepository.save(airport1);
+        airportRepository.save(airport2);
+
+        // WHEN
+        mockMvc.perform(get("/api/airport/options-for-input"))
+            // THEN
+            .andExpect(status().isOk())
+            .andExpect(content().json("""
+                [
+                    {
+                        "code": "GDN",
+                        "name": "Gdansk, Poland"
+                    },
+                    {
+                        "code": "GOT",
+                        "name": "Goteborg, Sweden"
+                    }
+                ]
+            """));
     }
 }
