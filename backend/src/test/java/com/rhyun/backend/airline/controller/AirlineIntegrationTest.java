56b3c27209cbebf0f0fc0b7dda9406baa8f05722
Rinae Hyun <rnhyun.dream@gmail.com>
2024-09-07 21:15:50 +0200

    Write unit and integration tests for getAllAirlines()

---
+++ backend/src/test/java/com/rhyun/backend/airline/controller/AirlineIntegrationTest.java
@@ -0,0 +1,70 @@
+package com.rhyun.backend.airline.controller;
+
+import com.rhyun.backend.airline.model.Airline;
+import com.rhyun.backend.airline.repository.AirlineRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class AirlineIntegrationTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private AirlineRepository airlineRepository;
+
+    Airline airline1 = new Airline("1", "airline", "SQ", "SIA",
+            "SINGAPORE AIRLINES", "SINGAPORE");
+    Airline airline2 = new Airline("2", "airline", "KE", "KAL",
+            "KOREAN AIR", "KOREAN AIR");
+
+    @Test
+    @DirtiesContext
+    void getAllAirlinesTest_whenDBIsEmpty_thenReturnEmptyList() throws Exception {
+        // GIVEN
+        // WHEN
+        mockMvc.perform(get("/api/airline"))
+            // THEN
+            .andExpect(status().isOk())
+            .andExpect(content().json("[]"));
+    }
+
+    @Test
+    @DirtiesContext
+    void getAllAirlinesTest_whenDBHasData_thenReturnListOfAirlines() throws Exception {
+        // GIVEN
+        airlineRepository.save(airline1);
+        airlineRepository.save(airline2);
+
+        // WHEN
+        mockMvc.perform(get("/api/airline"))
+            .andExpect(status().isOk())
+            .andExpect(content().json("""
+                    [{
+                        "id": "1",
+                        "type": "airline",
+                        "iataCode": "SQ",
+                        "icaoCode": "SIA",
+                        "businessName": "SINGAPORE AIRLINES",
+                        "commonName": "SINGAPORE"
+                    },
+                    {
+                        "id": "2",
+                        "type": "airline",
+                        "iataCode": "KE",
+                        "icaoCode": "KAL",
+                        "businessName": "KOREAN AIR",
+                        "commonName": "KOREAN AIR"
+                    }]
+                """));
+    }
+}
---
+++ backend/src/test/java/com/rhyun/backend/airline/service/AirlineServiceTest.java
@@ -0,0 +1,52 @@
+package com.rhyun.backend.airline.service;
+
+import com.rhyun.backend.airline.model.Airline;
+import com.rhyun.backend.airline.repository.AirlineRepository;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+class AirlineServiceTest {
+
+    private final AirlineRepository airlineRepository = mock(AirlineRepository.class);
+    private final AirlineService airlineService = new AirlineService(airlineRepository);
+
+    Airline airline1 = new Airline("1", "airline", "SQ", "SIA",
+            "SINGAPORE AIRLINES", "SINGAPORE");
+    Airline airline2 = new Airline("2", "airline", "KE", "KAL",
+            "KOREAN AIR", "KOREAN AIR");
+
+    @Test
+    void getAllAirlinesTest_whenDBIsEmpty_thenReturnEmptyList() {
+        // GIVEN
+        List<Airline> airlines = new ArrayList<>();
+        when(airlineRepository.findAll()).thenReturn(airlines);
+
+        // WHEN
+        List<Airline> actual = airlineService.getAllAirlines();
+
+        // THEN
+        assertThat(actual).isEmpty();
+        verify(airlineRepository, times(1)).findAll();
+    }
+
+    @Test
+    void getAllAirlinesTest_whenDBHasData_thenReturnListOfAirlines() {
+        // GIVEN
+        List<Airline> airlines = List.of(airline1, airline2);
+        when(airlineRepository.findAll()).thenReturn(airlines);
+
+        // WHEN
+        List<Airline> actual = airlineService.getAllAirlines();
+
+        // THEN
+        List<Airline> expected = List.of(airline1, airline2);
+        assertEquals(expected, actual);
+        verify(airlineRepository, times(1)).findAll();
+    }
+}
