ba2443a2dc5b191e01ef106364375bbe84632774
Rinae Hyun <rnhyun.dream@gmail.com>
2024-09-08 08:57:22 +0200

    Write unit test for getAirlineOptions() and the method itself

--- backend/src/main/java/com/rhyun/backend/airline/service/AirlineService.java
+++ backend/src/main/java/com/rhyun/backend/airline/service/AirlineService.java
@@ -1,9 +1,12 @@
 package com.rhyun.backend.airline.service;
 
+import com.rhyun.backend.airline.dto.GetAirlineDto;
 import com.rhyun.backend.airline.model.Airline;
 import com.rhyun.backend.airline.repository.AirlineRepository;
+import com.rhyun.backend.utils.StringHelper;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -17,6 +20,19 @@ public class AirlineService {
 
     public List<Airline> getAllAirlines() {
         return airlineRepository.findAll();
+    }
+
+    public List<GetAirlineDto> getAirlineOptions() {
+        List<Airline> airlines = getAllAirlines();
+
+        List<GetAirlineDto> airlineOptions = new ArrayList<>();
+
+        for (Airline airline : airlines) {
+            String name = StringHelper.capitalizeFirstLetter(airline.businessName());
+            airlineOptions.add(new GetAirlineDto(airline.iataCode(), name));
+        }
+
+        return airlineOptions;
     }
 
 }
--- backend/src/test/java/com/rhyun/backend/airline/service/AirlineServiceTest.java
+++ backend/src/test/java/com/rhyun/backend/airline/service/AirlineServiceTest.java
@@ -1,5 +1,6 @@
 package com.rhyun.backend.airline.service;
 
+import com.rhyun.backend.airline.dto.GetAirlineDto;
 import com.rhyun.backend.airline.model.Airline;
 import com.rhyun.backend.airline.repository.AirlineRepository;
 import org.junit.jupiter.api.Test;
@@ -46,6 +47,38 @@ class AirlineServiceTest {
 
         // THEN
         List<Airline> expected = List.of(airline1, airline2);
+        assertEquals(expected, actual);
+        verify(airlineRepository, times(1)).findAll();
+    }
+
+    @Test
+    void getAirlineOptionsTest_whenDBIsEmpty_thenReturnEmptyList() {
+        // GIVEN
+        List<Airline> airlines = new ArrayList<>();
+        when(airlineRepository.findAll()).thenReturn(airlines);
+
+        // WHEN
+        List<GetAirlineDto> actual = airlineService.getAirlineOptions();
+
+        // THEN
+        assertThat(actual).isEmpty();
+        verify(airlineRepository, times(1)).findAll();
+    }
+
+    @Test
+    void getAirlineOptionsTest_whenDBHasData_thenReturnListOfAirlines() {
+        // GIVEN
+        List<Airline> airlines = List.of(airline1, airline2);
+        when(airlineRepository.findAll()).thenReturn(airlines);
+
+        // WHEN
+        List<GetAirlineDto> actual = airlineService.getAirlineOptions();
+
+        // THEN
+        GetAirlineDto airlineDto1 = new GetAirlineDto("SQ", "Singapore Airlines");
+        GetAirlineDto airlineDto2 = new GetAirlineDto("KE", "Korean Air");
+        List<GetAirlineDto> expected = List.of(airlineDto1, airlineDto2);
+
         assertEquals(expected, actual);
         verify(airlineRepository, times(1)).findAll();
     }
