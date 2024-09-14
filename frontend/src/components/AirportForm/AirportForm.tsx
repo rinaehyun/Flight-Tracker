eb06326b29080d914fa78cec28c0dd4f7b85e168
Rinae Hyun <rnhyun.dream@gmail.com>
2024-09-14 23:17:09 +0200

    Create AirportForm template and add a NewAirport type

---
+++ frontend/src/components/AirportForm/AirportForm.tsx
@@ -0,0 +1,170 @@
+import './AirportForm.css'
+import {InputAdornment, SelectChangeEvent, TextField} from "@mui/material";
+import {NewAirport} from "../../types/model/dataType.ts";
+import {ChangeEvent, Dispatch, SetStateAction} from "react";
+import {FlightStatus} from "../../types/enum.ts";
+
+type AirportFormProps = {
+    newAirport: NewAirport,
+    setNewAirport: Dispatch<SetStateAction<NewAirport>>,
+    buttonLabel: string
+}
+
+export default function AirportForm({ newAirport, setNewAirport, buttonLabel}: Readonly<AirportFormProps>) {
+
+    const handleChange = (event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement> |
+        ChangeEvent<HTMLSelectElement> | SelectChangeEvent<FlightStatus>) => {
+        const { name, value } = event.target;
+
+        if (name === "latitude" || name === "longitude") {
+            setNewAirport((prev) => ({
+                ...prev,
+                geoCode: {
+                    ...prev.geoCode,
+                    [name]: value
+                }
+            }));
+        } else if (name === "countryName" || name === "countryCode" || name === "regionCode") {
+            setNewAirport((prev) => ({
+                ...prev,
+                address: {
+                    ...prev.address,
+                    [name]: value
+                }
+            }));
+        } else if (name === "offSet") {
+            setNewAirport((prev) => ({
+                ...prev,
+                timeZone: {
+                    ...prev.timeZone,
+                    offSet: value
+                }
+            }));
+        } else {
+            setNewAirport((prev) => ({
+                ...prev,
+                [name]: value
+            }));
+        }
+        console.log(newAirport);
+    };
+
+    return (
+        <form className={"add-airport-form"} onSubmit={() => {}}>
+            <TextField
+                required
+                id={"outlined-basic"}
+                label={"Region"}
+                variant={"standard"}
+                placeholder={""}
+                color={"primary"}
+                sx={{width: "100%"}}
+                name={"regionCode"}
+                value={newAirport.address.regionCode}
+                onChange={handleChange}
+                autoComplete={"off"}
+            />
+            <TextField
+                required
+                id={"outlined-basic"}
+                label={"Country"}
+                variant={"standard"}
+                placeholder={"Netherlands"}
+                color={"primary"}
+                sx={{width: "100%"}}
+                name={"countryName"}
+                value={newAirport.address.countryName}
+                onChange={handleChange}
+                autoComplete={"off"}
+            />
+            <TextField
+                required
+                id={"outlined-basic"}
+                label={"Country Code"}
+                variant={"standard"}
+                placeholder={"NL"}
+                color={"primary"}
+                sx={{width: "100%"}}
+                name={"countryCode"}
+                value={newAirport.address.countryCode}
+                onChange={handleChange}
+                autoComplete={"off"}
+            />
+            <TextField
+                required
+                id={"outlined-basic"}
+                label={"City"}
+                variant={"standard"}
+                placeholder={"Amsterdam"}
+                color={"primary"}
+                sx={{width: "100%"}}
+                name={"name"}
+                value={newAirport.name}
+                onChange={handleChange}
+                autoComplete={"off"}
+            />
+            <TextField
+                required
+                id={"outlined-basic"}
+                label={"IATA Code"}
+                variant={"standard"}
+                placeholder={"AMS"}
+                color={"primary"}
+                sx={{width: "100%"}}
+                name={"iataCode"}
+                value={newAirport.iataCode}
+                onChange={handleChange}
+                autoComplete={"off"}
+            />
+            <div style={{ display: "flex", justifyContent: "space-between" }}>
+                <TextField
+                    label={""}
+                    id={"standard-start-adornment"}
+                    sx={{ m: 1, width: '17ch', marginLeft: 0, marginTop: "15px" }}
+                    slotProps={{
+                        input: {
+                            startAdornment: <InputAdornment position="start">Latitude</InputAdornment>,
+                        },
+                    }}
+                    variant={"standard"}
+                    onChange={handleChange}
+                    name={"latitude"}
+                    value={newAirport.geoCode.latitude}
+                    autoComplete={"off"}
+                />
+                <TextField
+                    label={""}
+                    id={"standard-start-adornment"}
+                    sx={{ m: 1, width: '17ch', marginRight: 0, marginTop: "15px" }}
+                    slotProps={{
+                        input: {
+                            startAdornment: <InputAdornment position="start">Longitude</InputAdornment>,
+                        },
+                    }}
+                    variant={"standard"}
+                    onChange={handleChange}
+                    name={"longitude"}
+                    value={newAirport.geoCode.longitude}
+                    autoComplete={"off"}
+                />
+            </div>
+            <TextField
+                required
+                id={"outlined-basic"}
+                label={"Time Zone"}
+                variant={"standard"}
+                placeholder={""}
+                color={"primary"}
+                sx={{width: "100%"}}
+                name={"offSet"}
+                value={newAirport.timeZone.offSet}
+                onChange={handleChange}
+                autoComplete={"off"}
+            />
+            <button
+                type={"submit"}
+                className={"airport-form-submit"}
+            >{buttonLabel}</button>
+        </form>
+    )
+}
--- frontend/src/pages/AirportPage/AddAirportPage/AddAirportPage.tsx
+++ frontend/src/pages/AirportPage/AddAirportPage/AddAirportPage.tsx
@@ -1,5 +1,49 @@
+import {Box} from "@mui/material";
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import {NavigateFunction, useNavigate} from "react-router-dom";
+import AirportForm from "../../../components/AirportForm/AirportForm.tsx";
+import {useState} from "react";
+import {NewAirport} from "../../../types/model/dataType.ts";
+
 export default function AddAirportPage() {
+    const [newAirport, setNewAirport] = useState<NewAirport>({
+        name: "",
+        iataCode: "",
+        geoCode: {
+            latitude: 0,
+            longitude: 0
+        },
+        address: {
+            countryName: "",
+            countryCode: "",
+            regionCode: ""
+        },
+        timeZone: {
+            offSet: ""
+        }
+    });
+
+    const navigate: NavigateFunction = useNavigate();
+
     return (
-        <p>Add airport info</p>
+        <div className={"add-flight-form-container"}>
+            <Box
+                sx={{
+                    display: 'flex',
+                    justifyContent: 'flex-start'
+                }}
+            >
+                <ArrowBackIcon
+                    sx={{cursor: "pointer"}}
+                    onClick={() => navigate(-1)}
+                />
+            </Box>
+            <h3>Add A New Airport</h3>
+            <AirportForm
+                newAirport={newAirport}
+                setNewAirport={setNewAirport}
+                buttonLabel={"Add a new airport"}
+            />
+        </div>
     )
 }
