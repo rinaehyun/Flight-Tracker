69f3e04291c461fc5dc59cf5bc04676b7d2cf3e6
Rinae Hyun <rnhyun.dream@gmail.com>
2024-10-10 13:40:37 +0200

    Add features of notification and disabled to Add/Edit Airline Forms in FE

--- frontend/src/pages/AirlinePage/EditAirlinePage/EditAirlinePage.tsx
+++ frontend/src/pages/AirlinePage/EditAirlinePage/EditAirlinePage.tsx
@@ -6,8 +6,12 @@ import {NewAirline} from "../../../types/model/dataType.ts";
 import {FormEvent, useEffect, useState} from "react";
 import axios from "axios";
 import {useNavigate, useParams} from "react-router-dom";
+import Notification from "../../../components/Notification/Notification.tsx";
+import {useNotificationTimer} from "../../../hooks/useNotificationTimer.ts";
 
 export default function EditAirlinePage() {
+    const [editable, setEditable ] = useState<boolean>(false);
+    const [showNotification, setShowNotification] = useState<boolean>(false);
     const [originalAirline, setOriginalAirline] = useState<NewAirline>({
         iataCode: "",
         businessName: "",
@@ -22,13 +26,11 @@ export default function EditAirlinePage() {
 
     const navigate = useNavigate();
     const params = useParams();
-    console.log(params);
     const id: string | undefined = params.id;
 
     const fetchAirline = () => {
         axios.get(`/api/airline/${id}`)
             .then(response => {
-                console.log(response)
                 setUpdatedAirline(response.data);
                 setOriginalAirline(response.data);
             })
@@ -40,14 +42,21 @@ export default function EditAirlinePage() {
     }, [])
 
     const onEdit = () => {
-        setUpdatedAirline(originalAirline);
+        if (id) {
+            setUpdatedAirline(originalAirline);
+        }
+        setEditable(!editable);
     }
 
     const updateAirline = (id: string, airline: NewAirline) => {
         axios.put(`/api/airline/${id}`, airline)
             .then(response => {
                 if (response.status === 200) {
-                    console.log("Airline data is updated!");
+                    setShowNotification(true);
+                    setTimeout(() => {
+                        setShowNotification(false);
+                        navigate("/airline");
+                    }, 4000);
                 }
             })
             .catch(error => console.error(error.response.data));
@@ -56,10 +65,20 @@ export default function EditAirlinePage() {
     const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
         event.preventDefault();
         if (id) updateAirline(id, updatedAirline);
+        setEditable(false);
     }
+
+    useNotificationTimer(showNotification, setShowNotification);
 
     return (
         <div className={"airline-edit-page"}>
+            {showNotification &&
+                <Notification
+                    setShowNotification={setShowNotification}
+                    message={"Airline data has been updated."}
+                    messageType={"success"}
+                />
+            }
             <div className={"top-buttons"}>
                 <Box
                     sx={{
@@ -72,7 +91,7 @@ export default function EditAirlinePage() {
                         onClick={() => navigate(-1)}
                     />
                 </Box>
-                <button onClick={onEdit}>Edit</button>g
+                <button onClick={onEdit}>{editable ? "Cancel Edit" : "Edit"}</button>
             </div>
             <h3>Edit Airline</h3>
             <AirlineForm
@@ -80,6 +99,7 @@ export default function EditAirlinePage() {
                 setNewAirline={setUpdatedAirline}
                 handleSubmit={handleSubmit}
                 buttonLabel={'Edit Airline'}
+                editable={editable}
             />
         </div>
     )
